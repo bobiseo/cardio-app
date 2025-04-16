763fc47c0cc93308354d6ec43f1046515c0e7de2
Mirae Seo <broadalfo@gmail.com>
2025-04-16 18:52:57 +0100

    fixxx

---
+++ save_clean_model.py
@@ -0,0 +1,23 @@
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingClassifier
+import joblib
+
+df = pd.read_csv("cardio.csv", sep=";")
+df["age"] = (df["age"] / 365).astype(int)
+df["bmi"] = df["weight"] / ((df["height"] / 100) ** 2)
+df.drop(["id", "height", "weight"], axis=1, inplace=True)
+
+X = df.drop("cardio", axis=1)
+y = df["cardio"]
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+
+model = GradientBoostingClassifier()
+model.fit(X_scaled, y)
+
+joblib.dump(model, "saved_models/model_gradient_boosting.pkl")
+joblib.dump(scaler, "saved_models/scaler.pkl")
